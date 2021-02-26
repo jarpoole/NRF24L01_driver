c10d143aa98c1c3c6c362e309947c2a749369117
jarpoole <43940867+jarpoole@users.noreply.github.com>
2021-02-26 16:29:18 +0000

    Deploying to main from @ jarpoole/NRF24L01_driver@a36bd07381c1403eca81b8d8dbb20c7e6962cef1 🚀

--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,12 +1,39 @@
 var indexSectionsWithContent =
 {
+  0: "dgnsu",
+  1: "n",
+  2: "n",
+  3: "n",
+  4: "dgsu",
+  5: "n",
+  6: "n",
+  7: "n",
+  8: "n"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros"
 };
 
