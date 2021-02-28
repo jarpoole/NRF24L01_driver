4dbfa1be31745bbabe5884242695b04f93519d9c
jarpoole <43940867+jarpoole@users.noreply.github.com>
2021-02-28 15:20:09 +0000

    Deploying to main from @ jarpoole/NRF24L01_driver@80acc01405cd13061e687b5f020b92c3932b1b6f 🚀

--- docs/html/search/all_1.js
+++ docs/html/search/all_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['callbacks',['callbacks',['../structnrf24l01__platform__t.html#a55b6b13ad3be9450dacc06cfee433ea2',1,'nrf24l01_platform_t']]],
-  ['check_5ffor_5finterrupt',['check_for_interrupt',['../structnrf24l01__platform__t.html#a14b4c6c1143a2d9fbe68c87883265808',1,'nrf24l01_platform_t']]],
-  ['commands',['Commands',['../group__commands.html',1,'']]]
+  ['check_5ffor_5finterrupt',['check_for_interrupt',['../structnrf24l01__platform__t.html#a14b4c6c1143a2d9fbe68c87883265808',1,'nrf24l01_platform_t']]]
 ];
