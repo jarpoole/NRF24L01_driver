0f7e4289ac1d4ee8990b9b904b094d8b443c18fe
jarpoole <43940867+jarpoole@users.noreply.github.com>
2021-03-23 21:29:49 +0000

    Deploying to main from @ jarpoole/NRF24L01_driver@62f5da952e3f6436a9393726ccc1e2195e178919 🚀

--- docs/html/search/all_6.js
+++ docs/html/search/all_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['platform_5fdeinit_292',['platform_deinit',['../structnrf24l01__platform__t.html#ae5b99c252f64ff39fa24ef88ec33800a',1,'nrf24l01_platform_t']]],
-  ['platform_5finit_293',['platform_init',['../structnrf24l01__platform__t.html#abe203f817fe86f25764587b59baa568f',1,'nrf24l01_platform_t']]]
+  ['platform_5fdeinit_290',['platform_deinit',['../structnrf24l01__platform__t.html#ae5b99c252f64ff39fa24ef88ec33800a',1,'nrf24l01_platform_t']]],
+  ['platform_5finit_291',['platform_init',['../structnrf24l01__platform__t.html#abe203f817fe86f25764587b59baa568f',1,'nrf24l01_platform_t']]]
 ];
--- docs/html/search/variables_0.js
+++ docs/html/search/variables_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['callbacks_354',['callbacks',['../structnrf24l01__platform__t.html#ae4382e8da69d0b669320b5009fb2df42',1,'nrf24l01_platform_t']]],
-  ['check_5ffor_5finterrupt_355',['check_for_interrupt',['../structnrf24l01__platform__t.html#a14b4c6c1143a2d9fbe68c87883265808',1,'nrf24l01_platform_t']]]
+  ['callbacks_352',['callbacks',['../structnrf24l01__platform__t.html#ae4382e8da69d0b669320b5009fb2df42',1,'nrf24l01_platform_t']]]
 ];
