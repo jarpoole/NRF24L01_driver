6f7bfee4c6158e7a5a6e91c3b25d596bf9f0f0de
jarpoole <43940867+jarpoole@users.noreply.github.com>
2021-03-24 21:23:32 +0000

    Deploying to main from @ jarpoole/NRF24L01_driver@d141cd51ad333a0c110c6ddcc5882572a898cf57 🚀

--- docs/html/search/groups_2.js
+++ docs/html/search/groups_2.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['spi_20commands_612',['SPI Commands',['../group__commands.html',1,'']]],
-  ['spi_20register_20addresses_613',['SPI Register Addresses',['../group__reg__addr.html',1,'']]],
-  ['spi_20register_20bit_20definitions_614',['SPI Register Bit Definitions',['../group__reg__bits.html',1,'']]]
+  ['pipe_5fapi_591',['Pipe_api',['../group__pipe__api.html',1,'']]]
 ];
