e1d1e4688ff7dc8ebf1b5666bddc920ce63c1da0
jarpoole <43940867+jarpoole@users.noreply.github.com>
2021-03-24 19:27:35 +0000

    Deploying to main from @ jarpoole/NRF24L01_driver@e965f586a07b460993e6281eb9127d4c90d0e923 🚀

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['spi_5fexchange_305',['spi_exchange',['../structnrf24l01__platform__t.html#adad84cbfcf4873ff02e12c9c0a156acf',1,'nrf24l01_platform_t']]]
+  ['spi_20commands_305',['SPI Commands',['../group__commands.html',1,'']]],
+  ['spi_20register_20addresses_306',['SPI Register Addresses',['../group__reg__addr.html',1,'']]],
+  ['spi_20register_20bit_20definitions_307',['SPI Register Bit Definitions',['../group__reg__bits.html',1,'']]],
+  ['spi_5fexchange_308',['spi_exchange',['../structnrf24l01__platform__t.html#adad84cbfcf4873ff02e12c9c0a156acf',1,'nrf24l01_platform_t']]]
 ];
--- docs/html/search/variables_4.js
+++ docs/html/search/variables_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['nrf24l01_5faddr_5fregs_371',['NRF24L01_ADDR_REGS',['../nrf24l01_8c.html#ae0720c1299008feb1ea63bf8785fe7d1',1,'nrf24l01.c']]],
-  ['nrf24l01_5frx_5fpw_5fpipe_372',['NRF24L01_RX_PW_PIPE',['../nrf24l01_8c.html#aa08ee4c813e302494e4d211fb0e86891',1,'nrf24l01.c']]]
+  ['nrf24l01_5faddr_5fregs_374',['NRF24L01_ADDR_REGS',['../nrf24l01_8c.html#ae0720c1299008feb1ea63bf8785fe7d1',1,'nrf24l01.c']]],
+  ['nrf24l01_5frx_5fpw_5fpipe_375',['NRF24L01_RX_PW_PIPE',['../nrf24l01_8c.html#aa08ee4c813e302494e4d211fb0e86891',1,'nrf24l01.c']]]
 ];
