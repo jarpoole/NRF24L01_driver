ea730a91804d58949a7e5d7e3201cac79932e581
jarpoole <43940867+jarpoole@users.noreply.github.com>
2021-03-24 22:05:10 +0000

    Deploying to main from @ jarpoole/NRF24L01_driver@e7b278984fe6aec17458e62104c6aa700f9541b4 🚀

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['spi_20commands_306',['SPI Commands',['../group__commands.html',1,'']]],
-  ['spi_20register_20addresses_307',['SPI Register Addresses',['../group__reg__addr.html',1,'']]],
-  ['spi_5fexchange_308',['spi_exchange',['../structnrf24l01__platform__t.html#adad84cbfcf4873ff02e12c9c0a156acf',1,'nrf24l01_platform_t']]]
+  ['spi_5fexchange_307',['spi_exchange',['../structnrf24l01__platform__t.html#adad84cbfcf4873ff02e12c9c0a156acf',1,'nrf24l01_platform_t']]]
 ];
